23bfd13c55bb933195b03fb284a31174c12007bf
Cezar Azevedo de Faveri <cazevedodefaveri@gmail.com>
2020-05-28 20:17:32 -0300

    Initial commit

---
+++ Assets/Scripts/SnapEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapEditor : MonoBehaviour
+{
+    void Update()
+    {
+        float sizeX = transform.localScale.x;
+        float posX = transform.localPosition.x;
+
+        
+        float sizeZ = transform.localScale.z;
+        float posZ = transform.localPosition.z;
+
+        float positionXSnap = Mathf.RoundToInt(posX / sizeX);
+        float positionZSnap = Mathf.RoundToInt(posZ / sizeZ);
+
+        transform.localPosition = new Vector3(positionXSnap, transform.localPosition.y, positionZSnap);
+    }
+}
